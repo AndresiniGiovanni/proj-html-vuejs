dd8b6d1fdea62319c4688d78329c49d6b1221ee8
AndresiniGiovanni <gio.andresini@hotmail.it>
2022-11-21 12:08:22 +0100

    scaffolding

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <header>
+      <HeaderComponent />
+    </header>
+    <main>
+      <MainComponent />
+    </main>
+
+    <footer>
+      <FooterComponent />
+    </footer>
+  </section>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
+import CardsComponent from "./components/CardsComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+import { store } from "./store";
+
+export default {
+  components: {
+    HeaderComponent,
+    MainComponent,
+    CardsComponent,
+    FooterComponent,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
